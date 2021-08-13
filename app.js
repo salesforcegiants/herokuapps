33ceb66b9526bc9d3d2710a676268862dc7ed8e1
salesforcegiants <salesforcegiants@gmail.com>
2021-08-13 05:34:14 -0700

    Initial Commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express=require('express');
+const app = express();
+const port= process.env.PORT;
+ 
+const{ Client } =require('pg');
+ 
+const client =new Client({
+connectionString: process.env.DATABASE_URL,
+ssl: true
+})
+client.connect();
+ 
+app.get('/',(req,res)=>{
+client.query('INSERT into Visits (created_at) values (NOW())', (err,response) => {
+ if(err) {
+throw err ;
+}
+return res ;
+})
+return res.send('Sucessfuly recorded the visit');
+})
+app.listen(port,() =>{ console.log('SERVER STARTED')}); 
